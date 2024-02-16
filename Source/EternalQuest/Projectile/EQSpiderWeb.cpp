155e6b721fa6f54d070fbf4c45d0680228f48db0
Hwangsangjin <hwangsangjin88@gmail.com>
2024-02-17 06:55:11 +0900

    데미지 받는 함수 접근 지정자 에러 수정

--- Source/EternalQuest/Projectile/EQSpiderWeb.cpp
+++ Source/EternalQuest/Projectile/EQSpiderWeb.cpp
@@ -55,12 +55,16 @@ void AEQSpiderWeb::Tick(float DeltaTime)
 void AEQSpiderWeb::OverLapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	 auto Player = Cast<AEQCharacterPlayer>(OtherActor);
+	/*auto Player = Cast<AEQCharacterPlayer>(OtherActor);
 	FDamageEvent DamageEvent;
 	if(Player != nullptr)
 	{
 		Player->TakeDamage(Damage,DamageEvent,nullptr,this);
 		this -> Destroy();
-	}
+	}*/
+
+	FDamageEvent DamageEvent;
+	OtherActor->TakeDamage(Damage, DamageEvent, nullptr, this);
+	this->Destroy();
 }
 
