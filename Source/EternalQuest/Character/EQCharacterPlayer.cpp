533d6bb5fe758875f5296c8290ac8ac6c84422e9
Hwangsangjin <hwangsangjin88@gmail.com>
2024-02-17 05:52:21 +0900

    공격 체크 인터페이스, 애님노티파이 추가

--- Source/EternalQuest/Character/EQCharacterPlayer.cpp
+++ Source/EternalQuest/Character/EQCharacterPlayer.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Character/EQCharacterPlayer.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "Components/BoxComponent.h"
@@ -10,11 +11,10 @@
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "Action/EQComponentMove.h"
-#include "Action/EQComponentAttack.h"
 #include "Action/EQComponentInteraction.h"
 #include "Action/EQComponentInventory.h"
 #include "Action/EQComponentMenuManager.h"
-#include "Kismet/GameplayStatics.h"
+#include "Component/EQComponentAttack.h"
 
 AEQCharacterPlayer::AEQCharacterPlayer()
 {
@@ -58,15 +58,44 @@ AEQCharacterPlayer::AEQCharacterPlayer()
 
 	// Component
 	MoveComp = CreateDefaultSubobject<UEQComponentMove>(TEXT("Move Component"));
-	AttackComp = CreateDefaultSubobject<UEQComponentAttack>(TEXT("Attack Component"));
 	InteractionComp = CreateDefaultSubobject<UEQComponentInteraction>(TEXT("Interaction Component"));
 	InventoryComp = CreateDefaultSubobject<UEQComponentInventory>(TEXT("Inventory Component"));
 	MenuManagerComp = CreateDefaultSubobject<UEQComponentMenuManager>(TEXT("MenuManager Component"));
+	AttackComp = CreateDefaultSubobject<UEQComponentAttack>(TEXT("Attack Component"));
 }
 
 void AEQCharacterPlayer::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void AEQCharacterPlayer::AttackHitCheck()
+{
+	AttackComp->HitCheck();
+}
+
+float AEQCharacterPlayer::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	SetDead();
+
+	return DamageAmount;
+}
+
+void AEQCharacterPlayer::SetDead()
+{
+	/*GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
+	PlayDeadAnimation();
+	SetActorEnableCollision(false);*/
+	Jump();
+}
+
+void AEQCharacterPlayer::PlayDeadAnimation()
+{
+	UAnimInstance* Animinstance = GetMesh()->GetAnimInstance();
+	Animinstance->StopAllMontages(0.0f);
+	Animinstance->Montage_Play(DeadMontage, 1.0f);
 }
 
 void AEQCharacterPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -89,22 +118,16 @@ void AEQCharacterPlayer::PossessedBy(AController* NewController)
 {
 	Super::PossessedBy(NewController);
 
-	APlayerController* PlayerController = CastChecked<APlayerController>(GetController());
-	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
 	{
-		Subsystem->AddMappingContext(DefaultMappingContext, 0);
-		//Subsystem->RemoveMappingContext(DefaultMappingContext);
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
 	}
 }
 
 void AEQCharacterPlayer::OnRep_Owner()
 {
 	Super::OnRep_Owner();
-
-	APlayerController* PlayerController = CastChecked<APlayerController>(GetController());
-	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-	{
-		Subsystem->AddMappingContext(DefaultMappingContext, 0);
-		//Subsystem->RemoveMappingContext(DefaultMappingContext);
-	}
 }
---
+++ Source/EternalQuest/Interface/EQInterfaceAnimationAttack.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interface/EQInterfaceAnimationAttack.h"
+
+// Add default functionality here for any IEQInterfaceAnimationAttack functions that are not pure virtual.
