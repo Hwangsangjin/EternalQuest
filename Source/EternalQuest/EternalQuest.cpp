0cc10df286ae340548c3b58643dc535208d450a6
Hwangsangjin <hwangsangjin88@gmail.com>
2024-02-19 12:34:18 +0900

    로그 매크로 추가

--- Source/EternalQuest/EternalQuest.cpp
+++ Source/EternalQuest/EternalQuest.cpp
@@ -3,4 +3,5 @@
 #include "EternalQuest.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(LogEternalQuest);
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EternalQuest, "EternalQuest" );
