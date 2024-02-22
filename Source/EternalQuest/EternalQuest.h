f847587199acd8c37a2dd78a6d339ebca9e3148f
Hwangsangjin <hwangsangjin88@gmail.com>
2024-02-22 16:32:41 +0900

    로그 매크로 추가

--- Source/EternalQuest/EternalQuest.h
+++ Source/EternalQuest/EternalQuest.h
@@ -4,8 +4,11 @@
 
 #include "CoreMinimal.h"
 
-#define NETMODEINFO_LOG ((GetNetMode() == ENetMode::NM_Client) ? *FString::Printf(TEXT("Client%d"), GPlayInEditorID) : ((GetNetMode() == ENetMode::NM_Standalone) ? TEXT("Standalone") : TEXT("Server")))
+#define LOCALROLEINFO_LOG *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetLocalRole()))
+#define REMOTEROLEINFO_LOG *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetRemoteRole()))
+#define NETMODEINFO_LOG ((GetNetMode() == ENetMode::NM_Client) ? *FString::Printf(TEXT("CLIENT%d"), GPlayInEditorID) : ((GetNetMode() == ENetMode::NM_Standalone) ? TEXT("STANDALONE") : TEXT("SERVER")))
 #define CALLINFO_LOG ANSI_TO_TCHAR(__FUNCTION__)
-#define EQ_LOG(LogCategory, Verbosity, Format, ...) UE_LOG(LogCategory, Verbosity, TEXT("[%s] %s %s"), NETMODEINFO_LOG, CALLINFO_LOG, *FString::Printf(Format, ##__VA_ARGS__))
+
+#define EQ_LOG(LogCategory, Verbosity, Format, ...) UE_LOG(LogCategory, Verbosity, TEXT("[%s][%s/%s] %s %s"), NETMODEINFO_LOG, LOCALROLEINFO_LOG, REMOTEROLEINFO_LOG, CALLINFO_LOG, *FString::Printf(Format, ##__VA_ARGS__))
 
 DECLARE_LOG_CATEGORY_EXTERN(LogEternalQuest, Log, All);
