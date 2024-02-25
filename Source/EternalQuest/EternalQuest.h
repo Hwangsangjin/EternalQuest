ee259e7902a4eb62515bcb6fe7891d8ade2baac4
Hwangsangjin <hwangsangjin88@gmail.com>
2024-02-26 01:20:55 +0900

    로그 매크로 추가

--- Source/EternalQuest/EternalQuest.h
+++ Source/EternalQuest/EternalQuest.h
@@ -6,9 +6,12 @@
 
 #define LOCALROLEINFO_LOG *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetLocalRole()))
 #define REMOTEROLEINFO_LOG *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetRemoteRole()))
+#define SUBLOCALROLEINFO_LOG *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetOwner()->GetLocalRole()))
+#define SUBREMOTEROLEINFO_LOG *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetOwner()->GetRemoteRole()))
 #define NETMODEINFO_LOG ((GetNetMode() == ENetMode::NM_Client) ? *FString::Printf(TEXT("CLIENT%d"), GPlayInEditorID) : ((GetNetMode() == ENetMode::NM_Standalone) ? TEXT("STANDALONE") : TEXT("SERVER")))
 #define CALLINFO_LOG ANSI_TO_TCHAR(__FUNCTION__)
 
 #define EQ_LOG(LogCategory, Verbosity, Format, ...) UE_LOG(LogCategory, Verbosity, TEXT("[%s][%s/%s] %s %s"), NETMODEINFO_LOG, LOCALROLEINFO_LOG, REMOTEROLEINFO_LOG, CALLINFO_LOG, *FString::Printf(Format, ##__VA_ARGS__))
+#define EQ_SUBLOG(LogCategory, Verbosity, Format, ...) UE_LOG(LogCategory, Verbosity, TEXT("[%s][%s/%s] %s %s"), NETMODEINFO_LOG, SUBLOCALROLEINFO_LOG, SUBREMOTEROLEINFO_LOG, CALLINFO_LOG, *FString::Printf(Format, ##__VA_ARGS__))
 
 DECLARE_LOG_CATEGORY_EXTERN(LogEternalQuest, Log, All);
