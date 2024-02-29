ce6e30ee337b53b37662cc9e07b0a36ac3182693
LDJ <ldj95412@gmail.com>
2024-02-29 20:48:13 +0900

    로그인 UI 디자인 수정 및 StateUI와의 연동

--- Source/EternalQuest/Widget/EQWidgetStateUI.cpp
+++ Source/EternalQuest/Widget/EQWidgetStateUI.cpp
@@ -4,13 +4,33 @@
 #include "Widget/EQWidgetStateUI.h"
 
 #include "Component/EQComponentStat.h"
+#include "Components/Image.h"
 #include "Components/ProgressBar.h"
+#include "Game/EQGameInstance.h"
 #include "GameFramework/Character.h"
-#include "Player/EQPlayerController.h"
+
+UEQWidgetStateUI::UEQWidgetStateUI(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	static ConstructorHelpers::FObjectFinder<UTexture2D> T_WarriorIconRef(TEXT("/Game/Assets/CleanFlatIcons/Textures/png_128/icon/icon_game/icon_game_163.icon_game_163"));
+	if (T_WarriorIconRef.Succeeded()) T_WarriorIcon = T_WarriorIconRef.Object;
+
+	static ConstructorHelpers::FObjectFinder<UTexture2D> T_MageIconRef(TEXT("/Game/Assets/CleanFlatIcons/Textures/png_128/icon/icon_game/icon_game_180.icon_game_180"));
+	if (T_MageIconRef.Succeeded()) T_MageIcon = T_MageIconRef.Object;
+}
 
 void UEQWidgetStateUI::NativeConstruct()
 {
 	Super::NativeConstruct();
+	auto ClassType = Cast<UEQGameInstance>(GetWorld()->GetGameInstance())->GetClassType();
+	if (ClassType == EClassType::ECT_Mage)
+	{
+		Img_JobClass->SetBrushFromTexture(T_MageIcon);
+	}
+	else if (ClassType == EClassType::ECT_Warrior)
+	{
+		Img_JobClass->SetBrushFromTexture(T_WarriorIcon);
+	}
 	StatComp = GetWorld()->GetFirstPlayerController()->GetCharacter()->FindComponentByClass<UEQComponentStat>();
 	StatComp->OnHpChanged.AddUObject(this, &ThisClass::UpdateStateUI);
 	SetMaxHP();
--- Source/EternalQuest/Widget/EQWidgetStateUI.h
+++ Source/EternalQuest/Widget/EQWidgetStateUI.h
@@ -6,6 +6,7 @@
 #include "Blueprint/UserWidget.h"
 #include "EQWidgetStateUI.generated.h"
 
+class UImage;
 class UEQComponentStat;
 class UTextBlock;
 class UProgressBar;
@@ -18,7 +19,7 @@ class ETERNALQUEST_API UEQWidgetStateUI : public UUserWidget
 	GENERATED_BODY()
 
 public:
-
+	UEQWidgetStateUI(const FObjectInitializer& ObjectInitializer);
 	virtual void NativeConstruct() override;
 
 	void UpdateStateUI(float InCurrentHP, float InMaxHP);
@@ -41,6 +42,15 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
 	TObjectPtr<UTextBlock> Txt_UserName;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
+	TObjectPtr<UImage> Img_JobClass;
+
 	UPROPERTY()
 	TObjectPtr<UEQComponentStat> StatComp;
+
+	UPROPERTY()
+	TObjectPtr<UTexture2D> T_WarriorIcon;
+
+	UPROPERTY()
+	TObjectPtr<UTexture2D> T_MageIcon;
 };
