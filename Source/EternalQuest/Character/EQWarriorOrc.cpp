9d9cd0dc7959619cd79c4374032916ad17aef6d5
KSE <fplls12@gmail.com>
2024-03-07 21:48:16 +0900

    오크 변경

--- Source/EternalQuest/Character/EQWarriorOrc.cpp
+++ Source/EternalQuest/Character/EQWarriorOrc.cpp
@@ -77,7 +77,7 @@ void AEQWarriorOrc::DropItem()
 		if (CurrItem)
 		{
 			CurrItem->ItemName.DataTable = ItemDataTable;
-			CurrItem->ItemName.RowName = TEXT("HealthPostion");
+			CurrItem->ItemName.RowName = TEXT("HealingPostion");
 			CurrItem->ItemType = EEQItemType::Consumtion;
 			CurrItem->ItemQuantity = 2;
 		}
